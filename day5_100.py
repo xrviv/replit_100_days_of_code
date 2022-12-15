08b007d56b948f9fbed1eb57e8e8d285f6ff4fa2
Daniel Andrei R. Garcia <44260360+xrviv@users.noreply.github.com>
2022-12-15 22:52:12 +0800

    Create day5_100.py

---
+++ day5_100.py
@@ -0,0 +1,20 @@
+print("Marvel Movie Character Creator")
+print("--")
+print()
+askHang = input("Do you like 'hanging around'?")
+if askHang == "Yes":
+	print("You must be Spiderman")
+else:
+	print("It would seem that you are not Spiderman.")
+print()
+askGravel = input("Do you have a 'gravelly' voice?")
+if askGravel == "Yes":
+	print("You are Korg.")
+else:
+	print("You are not Korg.")
+print()
+askMarvel = input("Do you often feel 'Marvelous'?")
+if askMarvel == "Yes":
+	print("Aha! You're Captain Marvel! Hi!")
+else:
+	print("Bummer, I don't know who you are.")
