3448b87ab4c48a954f293afbed7a151fd509fc78
Daniel Andrei R. Garcia <44260360+xrviv@users.noreply.github.com>
2022-12-15 22:43:26 +0800

    Create day1_100.py

---
+++ day1_100.py
@@ -0,0 +1,32 @@
+print(""" 
+Danny
+November 26th, 2022
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, 
+for a minimum of 10 minutes a day. 
+I'll be using Replit, an amazing online IDE so I can do thing 
+from my phone wherever I happen to be. No excuses for not coding
+from the middle of a field!""" )
+print("I am feeling 👌")
+print("You can follow my progress at replit.com/@dannybuntu") 
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("Hello Replit")
+print("I hope the statement above does not constitute a legally binding agreement.")
+print("The previous message uses Python syntax which is like this:")
+print("print(\"text\")")
+print("===========")
+print("In C++, we usually type: cout >> \"text\" >> endl; ")
+print("In Javascript, we usually type: console.log(\"text\")")
+print(""" 
+
+Come to think of it, I think this would look better:
+
+C++
+=================
+cout >> "text" >> endl;
+
+Javascript 
+=================
+console.log("text");
+
+""")
