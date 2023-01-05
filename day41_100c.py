503b0aa0ee4aadaa607634b10bb118a66d84e9c5
xrviv <dannyboygarcia@gmail.com>
2023-01-05 12:37:18 +0800

    Create day41_100c.py - input dics print

---
+++ day41_100c.py
@@ -0,0 +1,11 @@
+
+myDictionary = {"name" : "David the Mildy Terrifying", "health": 186, "strength": 4, "equipped":"l33t haxx0r p0werz"}
+
+for name, value in myDictionary.items():
+print(f"{name}: {value}")
+
+if (name == "strength"): 
+if value > 100: # This nested if wasn't indented properly
+  print("Whoa, SO STRONG")
+else:
+  print("Looks like you skipped leg day, arm day, chest day and, well, gym day entirely bro!")
