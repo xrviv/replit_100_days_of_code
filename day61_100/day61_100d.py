e9c778fa306cb273f8c42f258513f17c5e5426b4
xrviv <dannyboygarcia@gmail.com>
2023-02-08 01:09:38 +0800

    Created Day61 - diary, replit db

--- day61_100/day61_100d.py
+++ day61_100/day61_100d.py
@@ -1,34 +1,57 @@
-from replit import db
-import datetime, os, time
+#OpenAI GPT3 Assisted Code
 
-def addTweet():
-	tweet = input("Tweet > ")
-	timestamp = datetime.datetime.now()
-	key = f"mes {timestamp}"
-	db[key] = tweet
-	time.sleep(1)
-	os.system("clear")
-	
-def viewTweet():
-	matches = db.prefix("mes")
-	matches = matches[::-1]
-	counter = 0
-	for i in matches:
-		print(db[i])
-		print()
-		time.sleep(0.3)
-		counter+=1
-		if(counter%10==0):
-			carryOn = input("Next 10?: ")
-			if(carryOn.lower()=="no"):
-				break
-	time.sleep(1)
-	os.system("clear")
-	
-while True:
-	print("Tweeter")
-	menu = input("1: Add Tweet\n2: View Tweets\n> ")
-	if menu == "1":
-		addTweet()
-	else:
-		viewTweet()
+from replit import db
+import getpass, os, datetime, time
+
+password = "BananaPie"
+
+def wrongPass():
+    print("Incorrect Password. Exiting.")
+    exit
+
+def showMenu():
+    print()
+    print("📖 Welcome to Top Secret Journal")
+    print("1: Add diary entry\n2: View Diary entry\n3: Exit")
+    print()
+    choice = input("> ")
+    if choice == "1":
+      addEntry()
+    if choice == "2":
+      viewEntry()
+    if choice == "3":
+      exit
+
+def addEntry():
+    diaryEntry = input("Add an entry > ")
+    timestamp = datetime.datetime.now()
+    db[timestamp] = diaryEntry
+    time.sleep(1)
+    os.system("clear")
+    showMenu()
+
+def viewEntry():
+    keys = list(db.keys())
+    keys.sort(reverse=True)
+    i = 0
+    while i < len(keys):
+        print("-------------------------+")
+        print(f"{keys[i]}: {db[keys[i]]}")
+        print("-------------------------+")
+        i += 1
+        if i == len(keys):
+            break
+        view_previous = input("View previous entry? (yes/no) ")
+        if view_previous.lower() == "yes":
+            continue
+        if view_previous.lower() == "no":
+            showMenu()
+            break
+
+os.system('clear')
+passInput = getpass.getpass(prompt='Password: ', stream=None)
+if passInput != password:
+    wrongPass()
+elif passInput == password:
+    showMenu()
+    
